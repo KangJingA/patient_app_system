03ae870b077c8204f73e44f687df13919b23dbe4
KangJingA <66071629+KangJingA@users.noreply.github.com>
2021-07-13 23:47:00 +0800

    appointment schema

--- server/model/Appointment.js
+++ server/model/Appointment.js
@@ -1,1 +1,28 @@
 const mongoose = require("mongoose");
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const AppointmentSchema = new mongoose.Schema({
+    appointment_id: {
+        type: Number,
+    },
+    appointment_date_time: {
+        type: Date,
+        required: true,
+    },
+    doctor_id: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    patient_id: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    }
+})
+
+AppointmentSchema.plugin(AutoIncrement, {id:'order_seq',inc_field: 'appointment_id'});
+
+module.exports = mongoose.model('Appointment', AppointmentSchema);
