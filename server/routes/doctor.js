8c9052875e015fb946dace803e89a3a012277f5b
KangJingA <66071629+KangJingA@users.noreply.github.com>
2021-07-16 22:58:59 +0800

    parse doctor and patient data

--- server/routes/doctor.js
+++ server/routes/doctor.js
@@ -1,33 +1,25 @@
 const router = require("express").Router();
 const Doctor = require("../model/doctor");
 
+const parseDoctor = async (doctor) => {
+  return { doctor_id: doctor.doctor_id, doctor_name: doctor.doctor_name };
+};
+
 router.get("/", (req, res) => {
   res.send("doctor route is working");
 });
 
 router.post("/getDoctor", async (req, res) => {
-  //   Doctor.find({}).then(function (doctors) {
-  //     console.log(doctors);
-  //   });
-
   const doctor = await Doctor.findOne({ doctor_id: req.body.doctor_id });
-//   const doctor = await Doctor.findOne({ doctor_age: req.body.doctor_age, doctor_gender: req.body.doctor_gender });
-
 
   if (!doctor) {
     res.statusMessage = "doctor does not exist";
-    return res.status(403).end();
+    return res.status(403).end(); // 403 forbidden cos login
   }
-//   console.log(typeof doctor);
 
-//   const userDetails = {
-//     id: doctor.doctor_id,
-//     name: doctor.doctor_name,
-//     age: doctor.doctor_age,
-//     gender: doctor.doctor_gender,
-//   };
-//   console.log(userDetails);
-  res.send(doctor);
+  const parsedData = await parseDoctor(doctor);
+
+  res.send(parsedData);
 });
 
 module.exports = router;
--- server/routes/patient.js
+++ server/routes/patient.js
@@ -1,31 +1,24 @@
 const router = require("express").Router();
 const Patient = require("../model/Patient");
 
+const parsePatient = async (patient) => {
+  return { patient_id: patient.patient_id, patient_name: patient.patient_name };
+};
+
 router.get("/", (req, res) => {
   res.send("patient route is working");
 });
 
 router.post("/getPatient", async (req, res) => {
-  //   Patient.find({}).then(function (patients) {
-  //     console.log(patients);
-  //   });
-
   const patient = await Patient.findOne({ patient_id: req.body.patient_id });
 
   if (!patient) {
     res.statusMessage = "patient does not exist";
-    return res.status(403).end();
+    return res.status(403).end(); // 403 forbidden cos login
   }
-//   console.log(typeof patient);
 
-//   const userDetails = {
-//     id: patient.patient_id,
-//     name: patient.patient_name,
-//     age: patient.patient_age,
-//     gender: patient.patient_gender,
-//   };
-//   console.log(userDetails);
-  res.send(patient);
+  const parsedData = await parsePatient(patient);
+  res.send(parsedData);
 });
 
 module.exports = router;
