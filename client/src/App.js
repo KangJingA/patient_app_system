090530f9c2f3be884ff37b0e79592cc753fff272
KangJingA <66071629+KangJingA@users.noreply.github.com>
2021-07-16 22:47:28 +0800

    appointments-service.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import AppointmentsService from "./services/appointments-service";
 
 function App() {
+  useEffect(async () => {
+    let patientData = { patient_id: "P2" };
+    let result = await AppointmentsService.getPatientAppointments(patientData);
+    console.log(result);
+
+    let doctorData = { doctor_id: "D1" };
+    let result2 = await AppointmentsService.getDoctorAppointments(doctorData);
+    console.log(result2);
+
+    let doctorDateData = { doctor_id: "D1", date: "08 Apr 2018" };
+    let result3 = await AppointmentsService.getDoctorAppointmentsByDate(
+      doctorDateData
+    );
+    console.log(result3);
+
+    let fixAppointmentData = {
+      patient_id: "P3",
+      doctor_id: "D3",
+      date: "28 may 2012",
+      time: "12:00:00",
+    };
+
+    // let result4 = await AppointmentsService.fixAppointment(fixAppointmentData);
+    // console.log(result4);
+
+    let result5 = await AppointmentsService.deleteAppointment(fixAppointmentData);
+    console.log(result5)
+  });
   return (
     <div className="App">
-
+      <h1>Just a test</h1>
     </div>
   );
 }
---
+++ client/src/services/appointments-service.js
@@ -0,0 +1,107 @@
+const apiUrl = process.env.REACT_APP_apiEndpoint + "/appointment";
+
+const headers = {
+  "Content-type": "application/json; charset=UTF-8",
+  "Access-Control-Allow-Origin": "*",
+};
+
+const AppointmentsService = {
+  async getPatientAppointments(data) {
+    const url = apiUrl + "/patientAppointments";
+
+    let result = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+        return { errorMsg: "Error: getPatientAppointments" };
+      });
+
+    return result;
+  },
+
+  async getDoctorAppointments(data) {
+    const url = apiUrl + "/doctorAppointments";
+
+    let result = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+        return { errorMsg: "Error: getDoctorAppointments" };
+      });
+
+    return result;
+  },
+
+  async getDoctorAppointmentsByDate(data) {
+    const url = apiUrl + "/doctorAppointmentsByDate";
+
+    let result = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+        return { errorMsg: "Error: doctorAppointmentsByDate" };
+      });
+
+    return result;
+  },
+
+  async fixAppointment(data) {
+    const url = apiUrl + "/appointment";
+
+    let result = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.status == 400) return res.text();
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+        return { errorMsg: "Error: fixAppointment" };
+      });
+
+    return result;
+  },
+
+  async deleteAppointment(data) {
+    const url = apiUrl + "/appointment";
+
+    let result = await fetch(url, {
+      method: "DELETE",
+      headers: headers,
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (res.status == 400) return res.text();
+        return res.json();
+      })
+      .catch((err) => {
+        console.log(err);
+        return { errorMsg: "Error: deleteAppointment" };
+      });
+
+    return result;
+  },
+};
+
+export default AppointmentsService;
